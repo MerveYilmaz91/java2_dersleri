004871e0cd459b8f640668027e70ff8f444eda47
merve <mmerveozturk34@gmail.com>
2026-02-25 13:53:20 +0300

    added Examples2.java

---
+++ src/gun8/Examples2.java
@@ -0,0 +1,7 @@
+package gun8;
+
+public class Examples2 {
+    public static void main(String[] args) {
+        // commit and push examples
+    }
+}
--- src/gun8/GithupExamples.java
+++ src/gun8/GithupExamples.java
@@ -2,7 +2,6 @@ package gun8;
 
 public class GithupExamples {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        // Github a commit icin örnek class
     }
 }

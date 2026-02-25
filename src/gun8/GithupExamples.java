e4bda87eca76929c532c85ac5112bebc574f0657
merve <mmerveozturk34@gmail.com>
2026-02-25 12:23:17 +0300

    gun8 eklendi

---
+++ src/gun8/GithupExamples.java
@@ -0,0 +1,8 @@
+package gun8;
+
+public class GithupExamples {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}

43807b2c4ed475843c851b1163e6075916e667b4
merve <mmerveozturk34@gmail.com>
2026-03-11 14:47:47 +0300

    odev2_8

--- src/JavaOdev2/soru2_OtobusKarti.java
+++ src/JavaOdev2/soru2_OtobusKarti.java
@@ -1,5 +1,7 @@
 package JavaOdev2;
 
+import java.util.Scanner;
+
 public class soru2_OtobusKarti {
     public static void main(String[] args) {
         /**
@@ -12,5 +14,53 @@ public class soru2_OtobusKarti {
          * Ek olarak, gün isimlerini (Pzt-Sal-...) kullanarak en çok yolculuk yapılan günü ekrana yazdır.
          **/
 
+        Scanner scanner = new Scanner(System.in);
+
+        int [] yolculukSayisi = new int[7];
+        String [] gunler = {"pazartesi", "salı", "çarşamba", "persembe", "cuma", "cumartesi", "pazar"};
+
+        for (int i = 0; i < 7; i++){
+            System.out.println(gunler[i] + " günü kaç yolculuk yapıldı?");
+            yolculukSayisi[i] = scanner.nextInt();
+        }
+
+        System.out.println("kart türünü giriniz: tam / öğrenci / indirimli");
+
+        String kartTuru = scanner.next();
+
+        int ucret = 0;
+        if (kartTuru.equalsIgnoreCase("tam")){
+            ucret = 60;
+        } else if (kartTuru.equalsIgnoreCase("ogrenci")){
+            ucret = 30;
+        } else if (kartTuru.equalsIgnoreCase("indirimli")){
+            ucret = 40;
+        } else {
+            System.out.println("geçersiz kart turu.");
+        }
+
+        int toplamHarcama = 0;
+        for (int i = 0; i < 7; i++){
+            toplamHarcama += yolculukSayisi[i] * ucret;
+        }
+        System.out.println("toplam harcama: " + toplamHarcama);
+
+        System.out.println("mevcut bakiyenizi giriniz: ");
+        int bakiye = scanner.nextInt();
+
+        if (bakiye >= toplamHarcama){
+            System.out.println("kalan bakiye: " + (bakiye - toplamHarcama));
+        }  else {
+            System.out.println("yetersiz bakiye.eksik miktar: " + (toplamHarcama - bakiye));
+        }
+        int max = yolculukSayisi[0];
+        int index = 0;
+        for (int i = 1; i < 7; i++){
+            if (yolculukSayisi[i] > max){
+                max = yolculukSayisi[i];
+                index = i;
+            }
+        }
+        System.out.println("en çok yolculuk yapılan gün: " + gunler[index]);
     }
 }
--- src/JavaOdev2/soru8_KargoTakip.java
+++ src/JavaOdev2/soru8_KargoTakip.java
@@ -1,5 +1,7 @@
 package JavaOdev2;
 
+import java.util.Scanner;
+
 public class soru8_KargoTakip {
     public static void main(String[] args) {
         /**
@@ -9,8 +11,62 @@ public class soru8_KargoTakip {
          * Kullanıcıdan teslim tipi al: "ev", "ofis", "şube".
          * Teslim tipine göre ücret belirle.
          * Kullanıcıdan mesafeyi (km) al; 30 km üstüyse ek ücret uygula.
-         * Son olarak, her kodun ilk 2 karakterini yazdır ve kodlarda kaç tane harf kaç tane
-         * rakam olduğunu toplamda say.
+         * Son olarak, her kodun ilk 2 karakterini yazdır ve
+         * kodlarda kaç tane harf kaç tane rakam olduğunu toplamda say.
          **/
+
+        Scanner scanner = new Scanner(System.in);
+
+        String [] takipKodlari = new String[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("takip kodu giriniz: ");
+            takipKodlari[i] = scanner.nextLine().trim();
+
+            if (takipKodlari[i].length() != 10) {
+                System.out.println("uyarı: takip kodu 10 karakter olmalı");
+            }
+        }
+        System.out.println("teslim tipini giriniz: ev / ofis / şube");
+        String teslimTipi =  scanner.nextLine();
+
+        int ucret = 0;
+        if (teslimTipi.equalsIgnoreCase("ev")) {
+            ucret = 30;
+        } else if (teslimTipi.equalsIgnoreCase("ofis")) {
+            ucret = 20;
+        }else if (teslimTipi.equalsIgnoreCase("sube")) {
+            ucret = 25;
+        } else {
+            System.out.println("geçersiz teslim tipi.");
+        }
+
+        System.out.println("mesafeyi giriniz:");
+        int mesafe = scanner.nextInt();
+         if (mesafe > 30) {
+             System.out.println("30 km üstü mesafe, ek ücret eklendi.");
+             ucret += 10;
+
+         }
+        System.out.println("teslim ucreti " + ucret +   "TL" );
+
+         int harfSayisi = 0;
+         int rakamSayisi = 0;
+
+         for (int i = 1; i < takipKodlari.length; i++) {
+             System.out.println("ilk iki karakter: " + takipKodlari[i].substring(0,2));
+
+             for (int j = 1; j < takipKodlari[i].length(); j++) {
+                 char c = takipKodlari[i].charAt(j);
+                 if (Character.isLetter(c)) {
+                     harfSayisi++;
+
+                 } else if (Character.isDigit(c)) {
+                     rakamSayisi++;
+                 }
+             }
+         }
+        System.out.println("toplam harf: " + harfSayisi);
+         System.out.println("toplam rakam: " + rakamSayisi);
     }
 }
